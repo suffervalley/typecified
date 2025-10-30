10c6b57213935abb9e1a037ca3800b8fa33204ed
Faisal Amir <urmauur@gmail.com>
2025-10-30 20:37:17 +0700

    chore: update structure registry

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import Features from "@/components/features-4";
-import Features2 from "@/registry/blocks/features-4";
-
 export default function Home() {
-  return (
-    <div>
-      <Features />
-      <Features2 />
-    </div>
-  );
+  return <div></div>;
 }
