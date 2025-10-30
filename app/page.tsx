10ec0e4d201b5254f878ac099554ae61ba2c7ae2
Faisal Amir <urmauur@gmail.com>
2025-10-30 19:24:42 +0700

    chore: update heading features

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import Features from "@/components/features-4";
+import Features2 from "@/registry/blocks/features-4";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Features />
+      <Features2 />
+    </div>
+  );
 }
--- components/features-4.tsx
+++ components/features-4.tsx
@@ -13,7 +13,7 @@ export default function Features() {
       <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-16">
         <div className="relative z-10 mx-auto max-w-xl space-y-6 text-center md:space-y-12">
           <h2 className="text-balance text-4xl font-medium lg:text-5xl">
-            The foundation for creative teams management for Louis
+            The foundation for creative teams management
           </h2>
           <p>
             Lyra is evolving to be more than just the models. It supports an
